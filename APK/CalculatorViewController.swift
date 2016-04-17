c2edbe175880446dbdb4f6532cb6d29aa6432dd4
Oskar Smith <oskar@fonk.amsterdam>
2016-04-17 17:41:26 +0200

    Calculator UI and IBOutlets for buttons

---
+++ APK/CalculatorViewController.swift
@@ -0,0 +1,53 @@
+//
+//  ViewController.swift
+//  APK
+//
+//  Created by Oskar Smith on 17/04/16.
+//  Copyright © 2016 Oskar Smith. All rights reserved.
+//
+
+import UIKit
+
+class CalculatorViewController: UIViewController {
+
+    @IBOutlet weak var commaButton: UIButton!
+    @IBOutlet weak var zeroButton: UIButton!
+    @IBOutlet weak var oneButton: UIButton!
+    @IBOutlet weak var twoButton: UIButton!
+    @IBOutlet weak var threeButton: UIButton!
+    @IBOutlet weak var fourButton: UIButton!
+    @IBOutlet weak var fiveButton: UIButton!
+    @IBOutlet weak var sixButton: UIButton!
+    @IBOutlet weak var sevenButton: UIButton!
+    @IBOutlet weak var eightButton: UIButton!
+    @IBOutlet weak var nineButton: UIButton!
+    
+    @IBOutlet weak var eraseButton: UIButton!
+    
+    @IBOutlet weak var alkButton: UIButton!
+
+    @IBOutlet weak var volButton: UIButton!
+    @IBOutlet weak var sekButton: UIButton!
+    
+    @IBOutlet weak var resultButton: UIButton!
+    
+    @IBOutlet weak var inputLabel: UILabel!
+    
+    @IBAction func numberButtonDidPress(sender: UIButton) {
+        print(sender.tag)
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
