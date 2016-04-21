6081f8082146fa790e10784236d94d4487ed0d6d
Oskar Smith <oskar@fonk.amsterdam>
2016-04-21 12:25:38 +0200

    Restructuring and adding APK formula

--- APK/CalculatorViewController.swift
+++ APK/CalculatorViewController.swift
@@ -13,7 +13,13 @@ class CalculatorViewController: UIViewController
 
     @IBOutlet var displayInputs: [UITextField]!
 
-    var userIsInTheMiddleOfTypingANumber: Bool = false
+    var userIsInTheMiddleOfTypingANumber = false
+    var alcohol = 0
+    var volume = 0
+    var price = 0
+
+    
+    // Add digits to display
     
     @IBAction func appendDigit(sender: UIButton) {
         let digit = sender.currentTitle!
@@ -21,7 +27,7 @@ class CalculatorViewController: UIViewController
         for displayInput in displayInputs {
             if displayInput.tag == 0 {
                 if userIsInTheMiddleOfTypingANumber {
-                    displayInput.text = displayInput.text! + digit
+                    displayInput.text =  displayInput.text! + digit
                 } else {
                     displayInput.text = digit
                     userIsInTheMiddleOfTypingANumber = true
@@ -29,4 +35,42 @@ class CalculatorViewController: UIViewController
             }
         }
     }
+
+    
+    // Switches display
+
+    @IBAction func calculatorState(sender: UIButton) {
+        let state = sender.currentTitle!
+        userIsInTheMiddleOfTypingANumber = false
+        
+        switch state {
+//        case "alk":
+//            displayValue = alcohol
+//        case "ml":
+//        case "sek":
+        default: break
+        }
+    }
+    
+    
+    // Calculates APK
+    
+    var operandStack = Array<Float>()
+    
+    @IBAction func result()
+    {
+        if operandStack.count == 3 {
+            displayInputs[3] = (alcohol/100)*volume/price
+        }
+    }
+    
+//    var displayValue: Float {
+//        get {
+//            return NSNumberFormatter().numberFromString(displayInputs.text!)!.floatValue
+//        }
+//        set {
+//            displayInputs.text = "\(newValue)"
+//            userIsInTheMiddleOfTypingANumber = false
+//        }
+//    }
 }
