6853358c5fee140b39c6f9c27ea910765b948c6c
Oskar Smith <oskar@fonk.amsterdam>
2016-04-26 16:04:33 +0200

    Swithcing states works except for new inputs replacing the contents

--- APK/CalculatorViewController.swift
+++ APK/CalculatorViewController.swift
@@ -60,33 +60,6 @@ class CalculatorViewController: UIViewController {
         }
     }
 
-    // Remove digits from display
-    
-    @IBAction func eraseButtonDidPress(sender: UIButton) {
-        let currentString = self.displayInputs[sender.tag].text
-        
-        if currentString?.characters.count == 0 {
-            return
-        }
-        
-        let truncatedString = currentString!.substringToIndex(currentString!.endIndex.predecessor())
-        
-        switch calculatorState {
-        case .AlcoholInputState:
-            alcohol = truncatedString
-            displayInputs[sender.tag].text = (alcohol.characters.count == 0 ? "%" : alcohol)
-        case .VolumeInputState:
-            volume = truncatedString
-            displayInputs[sender.tag].text = (volume.characters.count == 0 ? "ml" : volume)
-        case .PriceInputState:
-            price = truncatedString
-            displayInputs[sender.tag].text = (price.characters.count == 0 ? "sek" : price)
-        default:
-            break
-        }
-
-    }
-    
     
     // Switches display
 
@@ -114,11 +87,44 @@ class CalculatorViewController: UIViewController {
         userIsInTheMiddleOfTypingANumber = false
         
         for displayInput in displayInputs {
+            
+            print(displayInput.text)
+            
             if displayInput.tag == calculatorState.rawValue {
+                print("show displayInput \(displayInput.tag)")
                 displayInput.hidden = false
             } else {
+                print("hide displayInput \(displayInput.tag)")
                 displayInput.hidden = true
             }
         }
     }
+    
+    // Remove digits from display
+    
+    @IBAction func eraseButtonDidPress(sender: UIButton) {
+        let currentString = self.displayInputs[sender.tag].text
+        
+        if currentString?.characters.count == 0 {
+            return
+        }
+        
+        let truncatedString = currentString!.substringToIndex(currentString!.endIndex.predecessor())
+        
+        switch calculatorState {
+        case .AlcoholInputState:
+            alcohol = truncatedString
+            displayInputs[sender.tag].text = (alcohol.characters.count == 0 ? "%" : alcohol)
+        case .VolumeInputState:
+            volume = truncatedString
+            displayInputs[sender.tag].text = (volume.characters.count == 0 ? "ml" : volume)
+        case .PriceInputState:
+            price = truncatedString
+            displayInputs[sender.tag].text = (price.characters.count == 0 ? "sek" : price)
+        default:
+            break
+        }
+        
+    }
+    
 }
