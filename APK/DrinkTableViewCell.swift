d47cc4ae21690e6125f6d43999d1a1ffbb86ce62
Oskar Smith <oskar@fonk.amsterdam>
2016-05-04 15:48:27 +0200

    Setting up table view

---
+++ APK/DrinkTableViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  DrinkTableViewCell.swift
+//  APK
+//
+//  Created by Oskar Smith on 04/05/16.
+//  Copyright © 2016 Oskar Smith. All rights reserved.
+//
+
+import UIKit
+
+class DrinkTableViewCell: UITableViewCell {
+    
+
+}
---
+++ APK/DrinksTableViewController.swift
@@ -0,0 +1,29 @@
+//
+//  DrinksTableViewController.swift
+//  APK
+//
+//  Created by Oskar Smith on 04/05/16.
+//  Copyright © 2016 Oskar Smith. All rights reserved.
+//
+
+import UIKit
+
+class DrinksTableViewController: UITableViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.estimatedRowHeight = 60
+        tableView.rowHeight = UITableViewAutomaticDimension
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCellWithIdentifier("DrinkCell") as UITableViewCell!
+        
+        return cell
+    }
+    
+    
+    
+}
